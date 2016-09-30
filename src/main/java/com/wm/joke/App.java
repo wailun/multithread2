34d581f043062792cd7f2877e0be95215522293d
wm <meng.wang.sh@dianping.com>
2016-09-30 17:58:52 +0800

    Initial commit

---
+++ src/main/java/com/wm/joke/App.java
@@ -0,0 +1,13 @@
+package com.wm.joke;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
